e9ee5ce1d56ff570d04950c4a3454ff20c7c3097
Jihan Shamas <jihan.shamas@gmail.com>
2025-02-04 17:18:49 +0100

    add navbar and footer with basic designs

--- Recipe-Book/src/App.jsx
+++ Recipe-Book/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./HomePage/Navbar";
-import Footer from "./HomePage/Footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
