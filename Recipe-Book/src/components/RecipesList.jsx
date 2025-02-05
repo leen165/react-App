12114c3263b3d1941e7f864fdded11f976934bc0
Jihan Shamas <jihan.shamas@gmail.com>
2025-02-05 17:15:35 +0100

    add css file for recipes component

--- Recipe-Book/src/components/RecipesList.jsx
+++ Recipe-Book/src/components/RecipesList.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import recipes from "../assets/recipes.json";
-
+import "./RecipeList.css"
 function RecipesList() {
-  return
-  <>
-    {recipes.map((recipe) => {
-      return <div>
-      <p>{recipe.name}</p>;
-      <p>{recipe.calories}</p>;
-      <img src={recipe.image}/>;
-      <p>{recipe.servings}</p>;
-
-      </div>
-    })}
-  </>;
+  return (
+    <>
+      {recipes.map((recipe) => {
+        return (
+          <div className="recipeList">
+            <p>{recipe.name}</p>
+            <p>{recipe.calories}</p>
+            <img src={recipe.image} />
+            <p>{recipe.servings}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 export default RecipesList;
